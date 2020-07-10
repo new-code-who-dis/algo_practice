3b9cc7063f0d9917a8a5864e5adeadbcbce4af73
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-10 14:53:36 +0200

    getting rid of that long ass reference name but still these tests feel verbose

--- tests/gcdtest.py
+++ tests/gcdtest.py
@@ -1,13 +1,13 @@
-import gcd.gcdish
+import gcd.gcdish as gcd
 import unittest
 
 class GCDTest(unittest.TestCase):
     def testGCDSmall(self):
         # i dont get why it can't find the relative path, i doubt there are any functions in unittest called calc_gcd
-        self.assertEqual(gcd.gcdish.calc_gcd(4,8), 4)
+        self.assertEqual(gcd.calc_gcd(4,8), 4)
     def testGCDLarge(self):
         # i dont get why it can't find the relative path, i doubt there are any functions in unittest called calc_gcd
-        self.assertEqual(gcd.gcdish.calc_gcd(3918848,1653264), 61232)
+        self.assertEqual(gcd.calc_gcd(3918848,1653264), 61232)
 
 if __name__ == '__main__':
     unittest.main()
