b3fba28401928fc9deef2617bab83738b7ef1972
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-15 12:15:51 +0200

    damn this used to be some remedial shit why's it taking me so long to get it back?

--- greedy_algunos/iwant7figures.py
+++ greedy_algunos/iwant7figures.py
@@ -3,21 +3,38 @@ def arrange_dat_coin(x):
     # i think converting from int to str to int again is actually shitay
     # wanna play with only using int
     bigCoin = x
-    if x < 11:
+    if x > 11:
         digits = splitDigits(x)
-        thinkPension(digits)
+        return thinkPension(digits)
         
     return bigCoin
 
 def splitDigits(x):
     digits = []
-        while x > 0:
-            last = x%10
-            digits.append(last)
-            x = (x - last)/10
+    while x > 0:
+        last = x%10
+        digits.append(last)
+        x = (x - last)/10
     return digits
 
 def thinkPension(x):
     # get number of digits like base like if len is 3 you want 100 to start with and then just multiple
-    bigBank = len(x)
+    count = 1
+    salary = 0
+    cx = x
+    for idx, val in enumerate(x):
+        biggest = val
+        biggestidx = idx
+        for cidx, cval in enumerate(cx):
+            if biggest < cval:
+                biggestidx = cidx
+        biggest = x[biggestidx]
+        cx[biggestidx] = 0
+        x[biggestidx] = 0
+        salary += biggest * pow(10, (len(x)-idx))
+    return salary
+
+
+
+
 
