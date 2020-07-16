f5c89ab8faa1b2375cf6b7eed71f73b5f8f73a6e
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-16 15:12:05 +0200

    im trying to see that green

--- tests/greed_algunostest.py
+++ tests/greed_algunostest.py
@@ -6,3 +6,5 @@ class MuneyTest(unittest.TestCase):
         self.assertEqual(chaching.arrange_dat_coin(1234567), 7654321)
     def testNigthmare(self):
         self.assertEqual(chaching.thinkBroke([4,3,2,1]), [1,2,3,4])
+    def testPoBrokeNFab(self):
+        self.assertEqual(chaching.thinkBroke([0,0,0,0,1]), [0,0,0,0,1])
