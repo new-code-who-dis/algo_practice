2c3b84e7a1d7b4dcd4e3ce3a68165eec5359a5e8
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-22 13:51:56 +0200

    the genius hath struckith minus edge case of course

--- array_searches/array_searches.py
+++ array_searches/array_searches.py
@@ -1,15 +1,23 @@
 def binary_search(array, value):
     #assuming she sorted
-    index = None
     bottom = 0
     top = len(array) - 1
-    # while (index is None) and top < len(array):
-    #     start = top // 2
-    #     if array[start] > value:
-    #         top = start
-    #     elif: array[start] < value:
-    #         bottom = start
-    return value
+    return creo_mass(bottom, top,array, value)
+def creo_mass(bottom, top, array, value):
+    if array[bottom] is value:
+        return bottom
+    elif array[top] is value:
+        return top
+    else:
+        start = (top - bottom)//2 + bottom
+        if array[start] > value:
+            return creo_mass(bottom, start, array, value)
+        else:
+            return creo_mass(start, top, array, value)
+
+
+
+
 
 
 
