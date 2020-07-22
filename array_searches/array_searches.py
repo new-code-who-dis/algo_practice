022097af2f687a2f3637721ebc0b5e24e6c0a719
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-22 14:41:15 +0200

    edge case curved

--- array_searches/array_searches.py
+++ array_searches/array_searches.py
@@ -3,13 +3,16 @@ def binary_search(array, value):
     bottom = 0
     top = len(array) - 1
     return creo_mass(bottom, top,array, value)
+
 def creo_mass(bottom, top, array, value):
-    if array[bottom] is value:
+    if array[bottom] == value:
         return bottom
-    elif array[top] is value:
+    elif array[top] == value:
         return top
     else:
         start = (top - bottom)//2 + bottom
+        if start == bottom:
+            return "NOT_FOUND"
         if array[start] > value:
             return creo_mass(bottom, start, array, value)
         else:
