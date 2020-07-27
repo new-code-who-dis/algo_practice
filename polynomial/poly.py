88fd172ff2d4329eec539f42f4bf1af4c95d6fea
new-code-who-dis <new.code.who.dis@gmail.com>
2020-07-27 14:33:41 +0200

    slowly but not so damn surely

--- polynomial/poly.py
+++ polynomial/poly.py
@@ -9,3 +9,39 @@ def mult_poly(p1, p2):
             else:
                 results[key] = result
     return results
+def mult_karatsuba_style(p1, p2):
+    #dafuq this is supposed to be recursive, why am i not doing that
+    p1_values_for_formula = calc_formula_values(p1)
+    p2_values_for_formula = calc_formula_values(p2)
+
+
+
+    return p1_values_for_formula
+    # do that basic addition thing with the results?
+
+def calc_formula_values(x):
+    half = len(x)//2
+
+    first_half = x[0:half]
+    second_half = x[half:]
+    
+    first_half_dict = create_d_vaules(first_half, half)
+    second_half_dict = create_d_vaules(second_half, half)
+    
+    dz = {0: first_half_dict, 1: second_half_dict}
+    return dz
+
+def create_d_vaules(half_array, half):
+    length = half
+    d_values = {}
+    for val in half_array: # wondering if the index of the element will always correspond to x indices, yes huh? ok think through it after getting this working
+        d_values[half - length] = val
+        length -= 1
+    return d_values
+
+
+
+
+
+
+    
--- tests/polytest.py
+++ tests/polytest.py
@@ -4,8 +4,10 @@ import unittest
 class PolyTest(unittest.TestCase):
     def testPolyNiave(self):
         self.assertEqual(poly.mult_poly([2,3,4],[3,4,5]),{4:20,3:31,2:34,1:17,0:6})
-    def testPolyNiaveVariableLength(self):
+    def testPolyNiaveVariableLengthNiave(self):
         self.assertEqual(poly.mult_poly([1,2],[1,2,3]),{3:6,2:7,1:4,0:1})
+    def testPolyKaratsuba(self):
+        self.assertEqual(poly.mult_karatsuba_style([1,2,3,4],[4,3,2,1]),{6:4,5:11,4:20,3:30,2:20,1:11,0:4})
 
 if __name__ == '__main__':
     unittest.main()
