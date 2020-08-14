d932a9a43a767e3e7d5ab9c94c5d83db5d3159e7
new-code-who-dis <new.code.who.dis@gmail.com>
2020-08-14 16:12:14 +0200

    changed result to list cause its more efficient

--- polynomial/poly.py
+++ polynomial/poly.py
@@ -1,10 +1,10 @@
 def mult_poly(p1, p2):
-    results = {}
+    results = [None]*(len(p1) + len(p2)-1)
     for idx, val in enumerate(p1):
         for idx2, valu2 in enumerate(p2):
             key = idx + idx2
             result = val*valu2
-            if key in results:
+            if results[key] is not None:
                 results[key] = results[key] + result # damn it really did seem like i didn't need the if here cause if the key doesn't exist an new entry is created
             else:
                 results[key] = result
--- tests/polytest.py
+++ tests/polytest.py
@@ -3,11 +3,13 @@ import unittest
 
 class PolyTest(unittest.TestCase):
     def testPolyNiave(self):
-        self.assertEqual(poly.mult_poly([2,3,4],[3,4,5]),{4:20,3:31,2:34,1:17,0:6})
+        self.assertEqual(poly.mult_poly([2,3,4],[3,4,5]),[6, 17, 34, 31, 20])
     def testPolyNiaveVariableLengthNiave(self):
-        self.assertEqual(poly.mult_poly([1,2],[1,2,3]),{3:6,2:7,1:4,0:1})
+        self.assertEqual(poly.mult_poly([1,2],[1,2,3]),[1, 4, 7, 6])
+    def testPolyNiaveOddPair(self):
+        self.assertEqual(poly.mult_poly([1,2,3,4],[4,3,2,1]),[4, 11, 20, 30, 20, 11, 4])
     def testPolyKaratsuba(self):
-        self.assertEqual(poly.mult_karatsuba_style([1,2,3,4],[4,3,2,1]),{6:4,5:11,4:20,3:30,2:20,1:11,0:4})
+        self.assertEqual(poly.mult_karatsuba_style([1,2,3,4],[4,3,2,1]),[4, 11, 20, 30, 20, 11, 4])
 
 if __name__ == '__main__':
     unittest.main()
