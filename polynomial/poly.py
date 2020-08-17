3531f2938bb6a8ca484b546b2875e47e8b3c1b3d
new-code-who-dis <new.code.who.dis@gmail.com>
2020-08-17 14:59:44 +0200

    much much closer, just gotta figure out the concat part with the new powers now

--- polynomial/poly.py
+++ polynomial/poly.py
@@ -10,21 +10,18 @@ def mult_poly(p1, p2):
                 results[key] = result
     return results
 
-def add_poly(p1,p2,p3,p4):
-    result = [None]*len(p1)
-    for i,val in p1:
-        result[i] = p1[i] + p2[i] + p3[i] + p4[i]
-    return result
 #make it variable length though? or just do adds in main for 2 at a time instead of 4
 def add_poly(p1,p2):
-    result = [None]*len(p1)
-    for i,val in p1:
+    length = len(p1)#expecting the lengths are the same at this point
+    result = [None]*length
+    for i in range(length):
         result[i] = p1[i] + p2[i]
     return result
 
 def sub_poly(p1,p2):
-    result = [None]*len(p1)
-    for i,val in p1:
+    length = len(p1) #too fucking simliar to add like is it faster to pass in an operation or should i make all the values in one list negative?
+    result = [None]*length
+    for i in range(length):
         result[i] = p1[i] - p2[i]
     return result
  
@@ -32,21 +29,24 @@ def mult_karatsuba_style(p1, p2):
     #deal with edge case where poly length is odd
     p1_half = len(p1)//2
     p1_d0 = calc_derivatives(p1,0,p1_half)
-    p1_d1 = calc_derivatives(p1,p1_half,(len(p1)-1))
+    p1_d1 = calc_derivatives(p1,p1_half,len(p1))
 
     p2_half = len(p2)//2
-    p2_e0 = calc_derivatives(p2,0,p2_half-1)
-    p2_e1 = calc_derivatives(p2,p2_half,(len(p2)-1))
+    p2_e0 = calc_derivatives(p2,0,p2_half)
+    p2_e1 = calc_derivatives(p2,p2_half,len(p2))
 
     # (d1*e1)x^2 + ( (d1+d0)+(e1+e0) - (d1*e1) - (d0*e0) )x + (d0*e0)
+    all_ds_add = add_poly(p1_d1, p1_d0) 
+    all_es_add = add_poly(p2_e0, p2_e1)
+    all_add = add_poly(all_ds_add, all_es_add)
+
     d1e1_mult = mult_poly(p1_d1, p2_e1)
     d0e0_mult = mult_poly(p1_d0, p2_e0)
-
-    all_add = add_poly(p1_d1, p1_d0, p2_e0, p2_e1)
     mults_add = add_poly(d1e1_mult, d0e0_mult)
     #maybe instead of another function i could just make all the values in a list negative
     #  and reuse the add method
     inner_sub = sub_poly(all_add, mults_add)
+
     return d0e0_mult  
 
 def calc_derivatives(poly, start, stop):
@@ -72,6 +72,11 @@ def calc_derivative_power(disposition,index,half):
         return 0
     else:
         return power
+
+#THOUGHTS
+# Wowie no method overloading in python, feeling hurt to say the least
+#
+
 
 
 
